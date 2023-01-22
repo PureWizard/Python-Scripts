4b5f46792cdda156489b15c2ca3ff45c3545a340
Salvatore <113462615+PureWizard@users.noreply.github.com>
2023-01-23 01:28:32 +0500

    Update Speak-Through-Cmd-line.py

--- Speak-Through-Cmd-line.py
+++ Speak-Through-Cmd-line.py
@@ -1,11 +1,12 @@
 # just write your message after running out it will automatically convert your text to speech
 from gtts import gTTS
 import os
+import subprocess
 
 def speak():
     sentence = input("Enter the text you want to be spoken: ")
     tts = gTTS(sentence)
     tts.save("output.mp3")
-    os.system("start output.mp3")
+    subprocess.Popen(['start', 'output.mp3'], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
 speak()
