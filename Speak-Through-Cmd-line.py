a46ae620f004edc25bdac3086889fdf7c633d698
Salvatore <113462615+PureWizard@users.noreply.github.com>
2023-01-23 01:25:40 +0500

    Update Speak-Through-Cmd-line.py

--- Speak-Through-Cmd-line.py
+++ Speak-Through-Cmd-line.py
@@ -1,10 +1,11 @@
 # just write your message after running out it will automatically convert your text to speech
-import pyttsx3
+from gtts import gTTS
+import os
 
 def speak():
-    engine = pyttsx3.init()
-    engine.setProperty('rate', -2)
-
     sentence = input("Enter the text you want to be spoken: ")
-    engine.say(sentence)
-    engine.runAndWait()
+    tts = gTTS(sentence)
+    tts.save("output.mp3")
+    os.system("start output.mp3")
+
+speak()
