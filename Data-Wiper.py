15e398b1d750db67795c98446e0c184201e903ae
Salvatore <113462615+PureWizard@users.noreply.github.com>
2023-01-24 03:15:21 -0800

    Add files via upload

---
+++ Data-Wiper.py
@@ -0,0 +1,24 @@
+# Author @PureWizard
+# Used to Wipe Data
+import os
+
+def get_random_bytes(size):
+    """
+    Generates random bytes of the specified size.
+    :param size: The number of bytes to generate
+    :return: A bytes object containing random bytes
+    """
+    return os.urandom(size)
+
+def wipe_data(file_path):
+    """
+    Wipes the data in the specified file using random bytes and the specified number of passes.
+    :param file_path: The file path to wipe
+    """
+    with open(file_path, 'wb') as f:
+        size = os.path.getsize(file_path)
+        for _ in range(3):
+            f.write(get_random_bytes(size))
+
+file_path = input("Enter the file path to wipe :) ")
+wipe_data(file_path)
